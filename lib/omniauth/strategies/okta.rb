3857d44a3d629b2f4d2c9333591014b3ee16f2e8
andrewvanbeek-okta <andrew.vanbeek@okta.com>
2018-06-02 19:08:28 -0500

    Update okta.rb

--- lib/omniauth/strategies/okta.rb
+++ lib/omniauth/strategies/okta.rb
@@ -46,12 +46,17 @@ module OmniAuth
           hash[:id_info] = validated_token(access_token.token)
         end
         print(hash)
+         print(hash)
+         print("efsiiosehfiohseiohn")
         hash
       end
 
       alias :oauth2_access_token :access_token
 
       def access_token
+          print(client)
+         print(client)
+         print("efsiiosehfiohseiohn")
         ::OAuth2::AccessToken.new(client, oauth2_access_token.token, {
           :expires_in => oauth2_access_token.expires_in,
           :expires_at => oauth2_access_token.expires_at
@@ -59,6 +64,9 @@ module OmniAuth
       end
 
       def raw_info
+        print(access_token)
+         print(access_token)
+         print("efsiiosehfiohseiohn")
         @_raw_info ||= access_token.get('/oauth2/v1/userinfo').parsed || {}
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
