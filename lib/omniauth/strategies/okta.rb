f6b6ffca959f47886f4fdc74a48db751d03e8dcb
andrewvanbeek-okta <andrew.vanbeek@okta.com>
2018-06-02 23:54:43 -0500

    Update okta.rb

--- lib/omniauth/strategies/okta.rb
+++ lib/omniauth/strategies/okta.rb
@@ -52,9 +52,6 @@ module OmniAuth
       alias :oauth2_access_token :access_token
 
       def access_token
-          print(client)
-         print(client)
-         print("efsiiosehfiohseiohn")
         ::OAuth2::AccessToken.new(client, oauth2_access_token.token, {
           :expires_in => oauth2_access_token.expires_in,
           :expires_at => oauth2_access_token.expires_at
